8694a9c68071140a378f626247f198d0b98ee119
Akeem <akeem.mighty@gmail.com>
2022-04-12 16:43:31 -0400

    Created Word Capitalizing Functionality

--- problemSolvingPt1.js
+++ problemSolvingPt1.js
@@ -10,3 +10,22 @@ function stringReverser(stringToReverse){
 let userStringToReverse = prompt("Please enter a string to reverse: ")
 let test = stringReverser(userStringToReverse);
 console.log(test);
+
+
+function capitalizeLetter(stringToCapitalize){
+    let arrayOfString = stringToCapitalize.split(" ");
+    let lengthOfArray = arrayOfString.length;
+    let resultingArray = [];
+    let firstElem = 0;
+    for(let i = 0; i < lengthOfArray; i++){
+        let tempVar = arrayOfString[i];
+        let firstLetter = tempVar[firstElem].toUpperCase();
+        let restOfWord = tempVar.slice(firstElem + 1);
+        tempVar =  firstLetter + restOfWord;
+        resultingArray.push(tempVar);
+    }
+    let capitalizedWord = resultingArray.join(" ");
+    return capitalizedWord;
+}
+let wordToCapitalize = prompt("Please enter words to which you would like to capitalize the first letter: ")
+console.log(capitalizeLetter(wordToCapitalize));
